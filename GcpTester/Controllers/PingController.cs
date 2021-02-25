abb74661d94b2e040c6d089a7987cc2ec704c6ec
Geetha Alapati <gxa82@meshdomain.ixicorp.com>
2021-02-25 17:16:58 -0500

    Add project files.

---
+++ GcpTester/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GcpTester.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
